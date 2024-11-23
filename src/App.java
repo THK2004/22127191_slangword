7e93ec3b4f82b18fe2e0ff1f017dd0eee0f7dda5
THK2004 <84148247+THK2004@users.noreply.github.com>
2024-11-23 22:47:11 +0700

    Fix name

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 import gui.guiClass;
 
-public class app {
+public class App {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
